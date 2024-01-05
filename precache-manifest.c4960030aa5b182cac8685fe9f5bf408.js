41e3fe483efc751bb7563a2a6dbd9e9bfb68a856
thovinak <thovinak@mcmaster.ca>
2024-01-05 01:40:48 -0500

    Updates

---
+++ precache-manifest.c4960030aa5b182cac8685fe9f5bf408.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "185607c9236760f0cd825c43398e9750",
+    "url": "/defi-react-api/index.html"
+  },
+  {
+    "revision": "954d1864e4d5e13a4032",
+    "url": "/defi-react-api/static/css/main.541233ab.chunk.css"
+  },
+  {
+    "revision": "5276cee4f4e1f3112290",
+    "url": "/defi-react-api/static/js/2.b21ab1c5.chunk.js"
+  },
+  {
+    "revision": "954d1864e4d5e13a4032",
+    "url": "/defi-react-api/static/js/main.49235a1e.chunk.js"
+  },
+  {
+    "revision": "3a5b36f2c40345d7e745",
+    "url": "/defi-react-api/static/js/runtime~main.b5596da2.js"
+  },
+  {
+    "revision": "5b6ea36acd204262af4d84e35ca4b8e8",
+    "url": "/defi-react-api/static/media/hero-img.5b6ea36a.png"
+  },
+  {
+    "revision": "583edf6ba94fb29bbfd3c87bc479f27b",
+    "url": "/defi-react-api/static/media/trade.583edf6b.png"
+  }
+]);
